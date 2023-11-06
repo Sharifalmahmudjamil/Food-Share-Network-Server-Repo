3a9b828c45c4c4395a14cd81ded73fb33b8e783b
sharifalmahmudjamil <smjamil111@gmail.com>
2023-11-06 00:48:34 -0800

    all data are sorting in server site

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app= express();
 const port = process.env.PORT || 5000;
 
@@ -34,10 +34,23 @@ async function run() {
     const foodCollection= client.db('foodDB').collection('food')
 
 
-    app.get('/addFood/sort',async(req,res)=>{
+    app.get('/addFood',async(req,res)=>{
       const cursor= foodCollection.find().sort({quantity : -1});
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.get('/availableFood/sort',async(req,res)=>{
+      const cursor=foodCollection.find().sort({date:1});
+      const result= await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/addFood/:id',async(req,res)=>{
+      const id= req.params.id;
+      const query= {_id: new ObjectId(id)};
+      const result= await foodCollection.findOne(query);
+      res.send(result)
     })
 
     app.post('/addFood', async(req,res)=>{
